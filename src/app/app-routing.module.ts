a3c98c87e2590ec4cf856fc12ccc5a8efc58df8a
Callum Fitt-Simpson <callum.fitt.simpson@gmail.com>
2020-03-18 18:17:49 +0000

    Add page not found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CalculatorComponent } from './calculator/calculator.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'calculator', component: CalculatorComponent },
+  {
+    path: '',
+    redirectTo: '/calculator',
+    pathMatch: 'full'
+  },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
