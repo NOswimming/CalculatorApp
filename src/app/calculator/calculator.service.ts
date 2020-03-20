8642cf225a1d494719504a02c741f9a48f478e18
Callum Fitt-Simpson <callum.fitt.simpson@gmail.com>
2020-03-20 01:01:35 +0000

    Add content type headers

--- src/app/calculator/calculator.service.ts
+++ src/app/calculator/calculator.service.ts
@@ -1,11 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
 
 @Injectable()
 export class CalculatorService {
   constructor(private http: HttpClient) { }
 
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json',
+    })
+    
+  };
+
   calculateString(calculationString: string) {
-    return this.http.post<number>('calculator', calculationString);
+    return this.http.post<number>('calculator', '"' + calculationString + '"', this.httpOptions);
   }
 }
